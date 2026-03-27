dc510fcf474cfbad4acde0af5c7f4d4e8811203f
Dhruv Suvarna <dhruv.suvarna@ingrammicro.com>
2026-03-27 08:58:42 +0530

    fix: addedd hardcoded backend url

--- clipart-frontend/services/api.ts
+++ clipart-frontend/services/api.ts
@@ -9,7 +9,7 @@ type GenerateResponse = {
   imageUrl: string;
 };
 
-const BACKEND_URL = process.env.BACKEND_URL;
+const BACKEND_URL = 'https://clipart-backend-n6hb.onrender.com';
 
 export const ApiService = {
   async generateClipart(payload: GenerateRequest): Promise<GenerateResponse> {
